f5ab643f257bb9bf1ed4a8a052f76f3933470ccc
Vaibhav Poriya <poriyavaibhav2611@gmail.com>
2025-09-30 12:52:38 +0530

    first commit

--- src/Components/Headers/Nav.jsx
+++ src/Components/Headers/Nav.jsx
@@ -1,43 +1,88 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
   return (
-
     <>
-        {/* Start Header/Navigation  */}
-		<nav className="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
+      {/* Start Header/Navigation  */}
+      <nav className="custom-navbar navbar navbar-expand-md navbar-dark bg-dark" aria-label="Furni navigation bar">
 
-			<div className="container">
-				<a className="navbar-brand" href="index.html">Furni<span>.</span></a>
+        <div className="container">
+          <NavLink className="navbar-brand" to="/">Furni<span>.</span></NavLink>
 
-				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-				<div className="collapse navbar-collapse" id="navbarsFurni">
-					<ul className="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
-						<li className="nav-item active">
-							<Link to="/" className="nav-link">Home</Link>
-						</li>
-						<li><Link to="/shop" className="nav-link">Shop</Link></li>
-						<li><Link to="/about" className="nav-link">About us</Link></li>
-						<li><Link to="/services" className="nav-link">Services</Link></li>
-						<li><Link to="/blog" className="nav-link">Blog</Link></li>
-						<li><Link to="/contact" className="nav-link">Contact us</Link></li>
-					</ul>
+          <div className="collapse navbar-collapse" id="navbarsFurni">
+            <ul className="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
+              <li>
+                <NavLink 
+                  to="/" 
+                  className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} 
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink 
+                  to="/shop" 
+                  className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} 
+                >
+                  Shop
+                </NavLink>
+              </li>
+              <li>
+                <NavLink 
+                  to="/about" 
+                  className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} 
+                >
+                  About us
+                </NavLink>
+              </li>
+              <li>
+                <NavLink 
+                  to="/services" 
+                  className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} 
+                >
+                  Services
+                </NavLink>
+              </li>
+              <li>
+                <NavLink 
+                  to="/blog" 
+                  className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} 
+                >
+                  Blog
+                </NavLink>
+              </li>
+              <li>
+                <NavLink 
+                  to="/contact" 
+                  className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} 
+                >
+                  Contact us
+                </NavLink>
+              </li>
+            </ul>
 
-					<ul className="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-						<li><Link className="nav-link" to="/login"><img src="assets/images/user.svg" /></Link></li>
-						<li><Link className="nav-link" to="/cart"><img src="assets/images/cart.svg" /></Link></li>
-					</ul>
-				</div>
-			</div>
-				
-		</nav>
-		{/* End Header/Navigation     */}
+            <ul className="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
+              <li>
+                <NavLink to="/login" className="nav-link">
+                  <img src="assets/images/user.svg" alt="user"/>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/cart" className="nav-link">
+                  <img src="assets/images/cart.svg" alt="cart"/>
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      {/* End Header/Navigation     */}
     </>
-
   )
 }
 
